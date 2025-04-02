99e7e7952941df5f3a293c9ad45946ce5049251f
Hugo Loiola <hugoloiola.1@gmail.com>
2025-04-02 10:03:55 -0300

    Classe Produto

---
+++ Loja/produto.py
@@ -0,0 +1,37 @@
+class Produto:
+    def __init__(self, id, nome, descricao, preco, quantidade):
+        self.id = id
+        self.nome = nome
+        self.descricao = descricao
+        self.preco = preco
+        self.quantidade = quantidade
+
+    def get_id(self):
+        return self.id
+
+    def set_id(self):
+        return self.id = id
+    
+    def get_nome(self):
+        return self.nome
+
+    def set_nome(self, nome):
+        self.nome = nome
+
+    def get_descricao(self):
+        return self.descricao
+    
+    def set_descricao(self, descricao):
+        self.descricao = descricao
+    
+    def get_preco(self):
+        return self.preco
+
+    def set_preco(self, preco):
+        self.preco = preco
+
+    def get_quantidade(self):
+        return self.quantidade
+    
+    def set_quantidade(self, quantidade):
+        self.quantidade = quantidade
