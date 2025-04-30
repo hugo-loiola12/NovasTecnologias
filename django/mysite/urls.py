50770234664586704eb6511e0a452af160eacba9
Hugo Loiola <hugoloiola.1@gmail.com>
2025-04-30 10:14:43 -0300

    feat: tailwind

--- django/mysite/urls.py
+++ django/mysite/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('sobre/', views.sobre, name='sobre'),
-    path('contato/', views.contato, name='contato') 
 ]
